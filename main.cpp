b0eb16e0f32ccc20589620f969a931c52f04b689
Ramnagendra2005 <168436878+Ramnagendra2005@users.noreply.github.com>
2024-10-21 22:31:39 +0530

    The primes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
  #include<bits/stdc++.h>
  using namespace std;
  int main(){
-    cout<<"Hello World"<<endl;
+    int a;
+    cin>>a;
+    for(int i=2;i<=a;i++){
+        if(a%i==0){
+            cout<<"false"<<endl;
+        }
+    }
+    cout<<"True"<<endl;
     return 0;
+
  }
